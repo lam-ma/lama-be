8e4c04d8d4d97cbd374c60525592025394a36f15
Mykhailo Kulak <mykhailo.kulak@olx.com>
2020-12-09 13:14:10 +0100

    adjust port via env

--- src/main/kotlin/com/lama/Main.kt
+++ src/main/kotlin/com/lama/Main.kt
@@ -6,10 +6,11 @@ import io.vertx.kotlin.coroutines.await
 val picUrl = "https://static.wixstatic.com/media/a612c9_44860425bc124b9390735854ed91f32e~mv2.gif"
 
 suspend fun main() {
+    val port = (System.getenv("LAMA_BE_PORT") ?: "8080").toInt()
     val vertx = Vertx.vertx()
     vertx.createHttpServer().requestHandler { req ->
         val body = """<html><body><img src="$picUrl" style="height: 100%"/></body></html>"""
         req.response().putHeader("content-type", "text/html").end(body)
-    }.listen(8080).await()
-    println("Server started at http://localhost:8080")
+    }.listen(port).await()
+    println("Server started at http://0.0.0.0:$port")
 }
