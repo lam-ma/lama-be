105a1e929473c08410725d5dd337287f8340b7ab
Mykhailo Kulak <mykhailo.kulak@olx.com>
2020-12-11 17:00:48 +0100

    cosmetics

--- src/main/kotlin/com/lama/HttpApi.kt
+++ src/main/kotlin/com/lama/HttpApi.kt
@@ -5,7 +5,7 @@ import com.lama.web.HttpApiBase
 import com.lama.web.HttpStatus
 import io.vertx.core.Vertx
 import io.vertx.ext.web.Router
-
+import io.vertx.ext.web.RoutingContext
 
 class HttpApi(
     vertx: Vertx,
@@ -28,39 +28,42 @@ class HttpApi(
             ctx.response().setStatusCode(HttpStatus.CREATED.code).endWithJson(quizz)
         }
         put("/quizzes/:id").handler { ctx ->
-            val quizzId = QuizzId(ctx.request().getParam("id"))
+            val quizzId = ctx.getQuizzId()
             val quizzDto = ctx.bodyAs<CreateQuizzDto>()
             val quizz = quizzService.edit(quizzId, quizzDto)
             ctx.response().endWithJson(quizz)
         }
         get("/quizzes/:id").handler { ctx ->
-            val quizzId = QuizzId(ctx.request().getParam("id"))
+            val quizzId = ctx.getQuizzId()
             val quizz = quizzService.get(quizzId)
             ctx.response().endWithJson(quizz)
         }
         post("/quizzes/:id/start").handler { ctx ->
-            val quizzId = QuizzId(ctx.request().getParam("id"))
+            val quizzId = ctx.getQuizzId()
             val game = gameService.startGame(quizzId)
             ctx.response().setStatusCode(HttpStatus.CREATED.code).endWithJson(game)
         }
         get("/games/:id").handler { ctx ->
-            val gameId = GameId(ctx.request().getParam("id"))
+            val gameId = ctx.getGameId()
             val game = gameService.get(gameId)
             ctx.response().endWithJson(game)
         }
         post("/games/:id").handler { ctx ->
-            val gameId = GameId(ctx.request().getParam("id"))
+            val gameId = ctx.getGameId()
             val stateChange = ctx.bodyAs<StateChange>()
             val updatedGame = gameService.update(gameId, stateChange)
             ctx.response().endWithJson(updatedGame)
         }
         get("/games/:id/scores").handler { ctx ->
-            val request = ctx.request()
-            val gameId = GameId(request.getParam("id"))
-            val limit = request.getParam("limit")?.toInt() ?: 5
+            val gameId = ctx.getGameId()
+            val limit = ctx.request().getParam("limit")?.toInt() ?: 5
             val score = gameService.getHighScore(gameId, limit)
             ctx.response().endWithJson(score)
         }
     }
+
+    private fun RoutingContext.getGameId() = GameId(request().getParam("id"))
+
+    private fun RoutingContext.getQuizzId() = QuizzId(request().getParam("id"))
 }
 
